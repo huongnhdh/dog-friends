e1c8bf3f554e6b91b57d204ff1360907bfa3a12f
huongnhd <huongnhd@lifull-tech.vn>
2019-04-25 14:42:46 +0700

    add github pages

---
+++ docs/precache-manifest.9b4a6e4b9834a88343f0c8c15d5e3778.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "a5575a14365d25d35d40",
+    "url": "/js/chunk-vendors.8f8da699.js"
+  },
+  {
+    "revision": "0364b9bb95021b7555e3",
+    "url": "/js/app.ea14a40f.js"
+  },
+  {
+    "revision": "de84d5ef7d9accee2c65e6bcf635fe3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f6d36c138430158326474af3ab7b95",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "d353f72d0295d202fb441d3c68d58752",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "0364b9bb95021b7555e3",
+    "url": "/css/app.dc9b0b8f.css"
+  },
+  {
+    "revision": "4a69130e57e4d781d16f5c3306395768",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "7ee0240fba70752122eb8585edb525ea",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "6d02fc12aec912d4aaca5c8ad38ead5e",
+    "url": "/android-chrome-192x192.png"
+  }
+];
