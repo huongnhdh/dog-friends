5d653045f3571d796651f3089eba0b20beb4cb02
huongnhdh <huong.nhdh@gmail.com>
2019-05-01 21:09:16 +0700

    fix env

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import router from "./router";
 require(`./assets/sass/main.scss`);
 
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
 
 new Vue({
